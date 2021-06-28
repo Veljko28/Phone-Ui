d13ac42b2e2356362d41dfae6cb7efd506a64094
Veljko28 <charlibear28@gmail.com>
2021-06-28 12:51:38 +0200

    tweaking the pages for layout ajustment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,17 @@ import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 import Head from 'next/head'
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (<Provider store={store}>
-        <Head>
-        <title>MobiStore - Online Mobile Store</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
+        <Layout>
+          <Head>
+          <title>MobiStore - Online Mobile Store</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
     </Provider>)
 }
 export default MyApp
