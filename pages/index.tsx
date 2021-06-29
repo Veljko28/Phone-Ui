eccd635c95176e307bbc8ca9d6eeb1c38e9a86be
Veljko28 <charlibear28@gmail.com>
2021-06-29 10:52:49 +0200

    new bootstrap css and image carousel to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import { Header } from "../components/Header"
-
+import { ImageCarousel } from "../components/ImageCarousel"
 
 export default function Home() {
   return (
-    <div >
+    <div>
+      {/* <ImageCarousel/> */}
     </div>
   )
 }
