45a7cf812fc0bd6aa847083b1d4a79b927114169
Veljko28 <charlibear28@gmail.com>
2021-06-30 13:01:17 +0200

    new constats for social and local links

---
+++ constants/Links.ts
@@ -0,0 +1,24 @@
+ const links = [
+    {
+      href: '/',
+      name: 'Home'
+    },
+    {
+      href: '/about',
+      name: 'About'
+    },
+      {
+      href: '/phones',
+      name: 'Phones'
+    },
+    {
+      href: '/contact',
+      name: 'Contact Us'
+    },
+    {
+      href: '/managment',
+      name: 'Managment'
+    },
+  ];
+
+  export default links;
---
+++ constants/SocialLinks.ts
@@ -0,0 +1,9 @@
+const socialLinks = [
+  'https://facebook.com/',
+  'https://twitter.com/',
+  'https://www.google.com/',
+  'https://instagram.com/',
+  'https://github.com/Veljko28',
+]
+
+export default socialLinks;
