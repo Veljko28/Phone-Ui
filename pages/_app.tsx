937e4fb27940609d0424e551c47aa45a77f9a95d
Veljko28 <charlibear28@gmail.com>
2021-06-30 13:00:56 +0200

    start new login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 import '../styles/Header.css';
+import '../styles/Footer.css';
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux';
 import store from '../redux/store';
---
+++ pages/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default Login;
