4771d0bf14e253b003b0e4574feae309335bc553
Veljko28 <charlibear28@gmail.com>
2021-07-03 11:57:46 +0200

    new styles in app and index subBar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import '../styles/globals.css';
 import '../styles/Header.css';
 import '../styles/Footer.css';
 import '../styles/Contact.css';
+import '../styles/Index.css';
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux';
 import store from '../redux/store';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import { LatestProducts } from "../components/FrontPage/LatestProducts"
 import { Grid } from "@material-ui/core";
+import SubBar from "../components/FrontPage/SubBar";
 // import { ImageCarousel } from "../components/ImageCarousel"
 
 export default function Home() {
   return (
     <Grid container justify="center">
       {/* <ImageCarousel/> */}
-        <Grid item>
           <LatestProducts/>
-        </Grid>
+          <SubBar />
     </Grid>
   )
 }
