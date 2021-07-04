7fc21ce937fc7905dcd6c80a0dcae8abf999742f
Veljko28 <charlibear28@gmail.com>
2021-07-04 11:43:49 +0200

    adding the new components to pages

--- pages/phones.tsx
+++ pages/phones.tsx
@@ -1,11 +1,17 @@
 import { Grid } from "@material-ui/core";
 import PhoneList from '../components/PhoneSearch/PhoneList';
+import CategoryBar from '../components/PhoneSearch/CategoryBar';
 
 const phones = () => {
 
     return ( 
-        <Grid container>    
-            <PhoneList/>
+        <Grid container>
+            <Grid item xs={3}>
+                <CategoryBar/>
+            </Grid> 
+            <Grid item xs={9}>
+                <PhoneList/>
+            </Grid> 
         </Grid>
     )
 }
