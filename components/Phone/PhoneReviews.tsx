68b9323fc229289a6def7eecf972ab3da24c8221
Veljko28 <charlibear28@gmail.com>
2021-07-07 21:09:36 +0200

    adding phone reviews

---
+++ components/Phone/PhoneReviews.tsx
@@ -0,0 +1,72 @@
+import { Grid, Typography, Button } from '@material-ui/core';
+import ColoredLine from '../../constants/ColoredLine';
+import Rating from '@material-ui/lab/Rating';
+
+const ReviewMap = ({id, rating, user, date, message}
+    : {id: string, rating: number, user: string, date: string, message: string}) => {
+
+        const hasLine = (id : string) => {
+            if (id !== '3'){
+                return <ColoredLine color="#eee"/>
+            }
+            else return "";
+        }
+
+    return (
+        <Grid container style={{padding: '10px'}} key={id}>
+            <div>
+            <Rating name="phone-rating" value={rating} precision={0.1} readOnly
+                     style={{fontSize: '16px', margin: '10px'}}/>
+                    <span style={{color: '#999', marginLeft: '10px'}}>By 
+                    <span style={{color: "#0cafe5"}}> {user} </span>on {date}</span>
+            </div>
+            <div style={{color: '#999', padding: '10px'}}>
+                {message}
+            </div>
+            {hasLine(id)}
+        </Grid>
+    );
+}
+
+const PhoneReviews = () => {
+
+    const reviews = [
+        {
+            id: '1',
+            rating: 5,
+            user: 'John Smith',
+            date: '4 July 2021',
+            message: "I'm so happy with the purchase the device was very clean " +
+            "and almost scratch free just as the photos indicated. I'll start by saying this is a very nice phone."
+            + "The 4 gb of RAM makes a huge difference in speed and overall performance,"
+        },
+        {
+            id: '2',
+            rating: 4,
+            user: 'Mary Sue',
+            date: '1 July 2021',
+            message: "I had the nexus 6p for a few years and I loved that phone. But I recently started" + 
+            " looking for a smaller, faster and newer alternative." +
+            " I love Google phones so pixel was the obvious choice. "
+        },
+        {
+            id: '3',
+            rating: 5,
+            user: 'Thomas Burr',
+            date: '27 Jun 2021',
+            message: "This was a replacement phone for a lost one. Didn’t want to pay full price." +
+            " This one was new and unlocked. Son has had it since February and it works great."
+        },
+    ]
+
+    return (
+        <Grid className="phone-details" container> 
+            <Typography variant="h6" style={{margin: '10px', marginLeft: '40px',
+            color: '#0cafe5'}}>Customer Reviews</Typography>
+            <ColoredLine color="#eee"/>
+            {reviews.map(x => ReviewMap(x))}
+        </Grid>
+    )
+}
+
+export default PhoneReviews;
