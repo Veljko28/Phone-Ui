8accd40e50ce243cdda18c93265a2a828ce03f7a
Veljko28 <charlibear28@gmail.com>
2021-07-08 11:41:03 +0200

    adding the redux login for removing items from cart

--- redux/actions/cartActions.ts
+++ redux/actions/cartActions.ts
@@ -1,10 +1,17 @@
 import { Phone } from "../../components/models/Phone";
-import templateAction from "../../constants/templateAction";
 
 export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+
 
 export const addToCart = ( payload: {item:  any
   , amount: number}) => ({
   type: ADD_TO_CART,
   payload
 });
+
+
+export const removeFromCart = (payload: Phone) => ({
+  type: REMOVE_FROM_CART,
+  payload
+})
--- redux/reducers/cartReducers.ts
+++ redux/reducers/cartReducers.ts
@@ -1,15 +1,15 @@
-import { ADD_TO_CART } from '../actions/cartActions';
+import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions/cartActions';
 import {Action} from '../reduxTypes';
 
 const initState = {
-    numOfItems: 0,
     items: []
 };
 
 
 const cartReducers = (state = initState , action: Action) => {
     switch (action.type){
-        case ADD_TO_CART: return {...state, numOfItems: state.numOfItems + parseInt(action.payload.amount), items: [...state.items, action.payload.item]  };
+        case ADD_TO_CART: return {...state, items: [...state.items, action.payload.item]  };
+        case REMOVE_FROM_CART: return {...state, items: state.items.filter(x => x !== action.payload)};
         default: return state;
     }
 }
