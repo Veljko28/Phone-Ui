3e5c4770403e263b1e03d8ef284f77e9188ba184
Veljko28 <charlibear28@gmail.com>
2021-07-08 13:08:08 +0200

    price details tab for cart

---
+++ components/Cart/PriceDetails.tsx
@@ -0,0 +1,35 @@
+import { Grid, Typography, Button} from '@material-ui/core';
+import ColoredLine from '../../constants/ColoredLine';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { State } from '../../redux/reduxTypes';
+
+const PriceDetails = () => {
+
+  const list = useSelector((state: State) => state.cart.items);
+
+  const totalPrice = list.length == 0 ? '0' : list.map(x => parseInt(x.price.slice(0,-1))).reduce((a,b) => a+b);
+
+  return (
+    <Grid container className="price-details">
+      <Typography variant="h6" style={{margin: '10px', marginLeft: '20px',
+        color: '#0cafe5'}}>Price Details</Typography>
+        <ColoredLine color="#eee"/>
+        <div className="price-info">
+          <Typography variant="subtitle2">
+            Price ({list.length} {list.length == 1 ? "item" : "items"}): <span style={{marginLeft: '15px'}}>{totalPrice + "$"}</span>
+          </Typography>
+          <Typography variant="subtitle2" style={{marginBottom: '10px'}}>
+              Delivery Charges: <span style={{color: '#43cf22', marginLeft: '15px'}}>Free</span>
+          </Typography>
+          <ColoredLine color="#eee" height="2px" />
+          <Typography variant="subtitle2" style={{marginTop: '10px'}}>
+            <strong>Amount Payable {totalPrice + "$"}</strong>
+          </Typography>
+          <Button style={{color: '#fff', backgroundColor: '#0cafe5', padding: '10px', marginTop: '15px'}}>Proceed To Checkout</Button>
+        </div>
+    </Grid>
+  )
+}
+
+export default PriceDetails
