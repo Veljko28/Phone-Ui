7a612ce05f436cca3ce494cbc9b460b1b1f6e940
Veljko28 <charlibear28@gmail.com>
2021-07-12 10:41:32 +0200

    creating the carousel and un mapping the category buttons for better optimatization

---
+++ components/FrontPage/CarouselMain.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import Image from 'next/image';
+
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+
+
+const CarouselMain = () => {
+  return (
+  <Carousel
+   interval={3000}
+   nextIcon={<ArrowForwardIosIcon style={{fontSize: '25px', color: 'black'}}/>}
+   prevIcon={<ArrowBackIosIcon style={{fontSize: '25px', color: 'black'}}/>}>
+
+    <Carousel.Item interval={1000}>
+      <Image
+        src="/slider/slider1.jpg"
+        alt="First slide"
+        width="100%"
+        layout="responsive"
+        height="35px"
+      />
+      <Carousel.Caption>
+        <div className="text-left">
+          <Image src="/slider/mi_logo.png" width="40px" height="40px"/>
+          <h3 style={{color: 'black'}}>Red Mi Y1</h3>
+          <p style={{color: 'black'}}>LED Selfie-light | Fingerprint sensor | Dedicated microSD card slot Snapdragon 435 octa-core processor</p>
+        </div>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item interval={500}>
+      <Image
+        src="/slider/slider2.jpg"
+        alt="Second slide"
+        width="100%"
+        layout="responsive"
+        height="35px"
+      />
+      <Carousel.Caption>
+        <div className="text-left">
+          <Image src="/slider/google_logo.png" width="40px" height="40px"/>
+          <h3 style={{color: 'black'}}>Google Pixel 2</h3>
+          <p style={{color: 'black'}}>The latest Qualcomm Snapdragon 835 processor | Water-resistant metal unibody | Up to 7 hours of battery.</p>
+        </div>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <Image
+        src="/slider/slider3.jpg"
+        alt="Third slide"
+        width="100%"
+        layout="responsive"
+        height="35px"
+      />
+      <Carousel.Caption>
+         <div className="text-left">
+          <Image src="/slider/apple_logo.png" width="40px" height="40px"/>
+          <h3 style={{color: 'black'}}>Iphone 8 Plus</h3>
+          <p style={{color: 'black'}}>5.5 inch Retina HD Display | 12MP wide-angle cameras</p>
+        </div>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
+  )
+}
+
+export default CarouselMain
--- components/FrontPage/CategoryButtons.tsx
+++ components/FrontPage/CategoryButtons.tsx
@@ -1,36 +1,105 @@
 import { Grid } from '@material-ui/core'
 import React from 'react'
 import Image from 'next/image';
+import Link from 'next/link';
+import { useDispatch } from 'react-redux';
+import { changeBrand } from '../../redux/actions/phonesActions';
 
-const BlockThree = (title: string, img: string ) => (
-  <Grid item xs={12} md={6} lg={3} className="phone-block" key={title}>
-    <div style={{margin: '20px'}}>
-      <Image src={title} width="75px" height="40px" />
-    </div>
-    <div style={{margin: '20px'}}>
-      <Image src={img} width="140px" height="140px" />
-    </div>
-  </Grid>
-)
+// const BlockThree = (title: string, img: string ) => (
+//   <Grid item xs={12} md={6} lg={3} className="phone-block" key={title}>
+//     <div style={{margin: '20px'}}>
+//       <Image src={title} width="75px" height="40px" />
+//     </div>
+//     <div style={{margin: '20px'}}>
+//       <Image src={img} width="140px" height="140px" />
+//     </div>
+//   </Grid>
+// )
 
 const CategoryButtons = () => {
+
+  const dispatch = useDispatch();
+
   return (
-    <Grid container style={{marginTop: '15px'}}>
-      {BlockThree("/front/nexus.png", "/front/display_img_1.png")}
-      {BlockThree("/front/samsung.png", "/front/display_img_3.png")}
-      <Grid item xs={12} md={12} lg={6} className="phone-block-big">
-        <div style={{margin: '20px'}}>
-          <Image src="/front/iphone.png" width="110px" height="40px" />
-        </div>
-        <div style={{margin: '20px', alignSelf: 'flex-end', marginLeft: '150px'}}>
-          <Image src="/front/display_img_2.png" width="210px" height="225px" />
-        </div>
+    <Link href="/phones/1">
+      <Grid container style={{marginTop: '15px'}}>
+
+        <Grid item xs={12} md={12} lg={3} className="phone-block"
+        onClick={() => dispatch(changeBrand('Google'))}>
+            <div style={{margin: '20px'}}>
+              <Image src="/front/nexus.png" width="75px" height="40px" />
+            </div>
+            <div style={{margin: '20px'}}>
+              <Image src="/front/display_img_1.png" width="140px" height="140px" />
+            </div>
+        </Grid>
+
+        <Grid item xs={12} md={12} lg={6} className="phone-block-big"
+        onClick={() => dispatch(changeBrand('apple'))}>
+          <div style={{margin: '20px'}}>
+            <Image src="/front/iphone.png" width="110px" height="40px" />
+          </div>
+          <div style={{margin: '20px', alignSelf: 'flex-end', marginLeft: '150px'}}>
+            <Image src="/front/display_img_2.png" width="210px" height="225px" />
+          </div>
+        </Grid>
+
+        <Grid item xs={12} md={12} lg={3} className="phone-block"
+        onClick={() => dispatch(changeBrand('samsung'))}>
+            <div style={{margin: '20px'}}>
+              <Image src="/front/samsung.png" width="75px" height="35px" />
+            </div>
+            <div style={{margin: '20px'}}>
+              <Image src="/front/display_img_3.png" width="140px" height="140px" />
+            </div>
+        </Grid>
+
+
+        <Grid item xs={12} md={6} lg={3} className="phone-block"
+        onClick={() => dispatch(changeBrand('htc'))}>
+            <div style={{margin: '20px'}}>
+              <Image src="/front/htc.png" width="75px" height="40px" />
+            </div>
+            <div style={{margin: '20px'}}>
+              <Image src="/front/display_img_4.png" width="140px" height="140px" />
+            </div>
+        </Grid>
+
+        <Grid item xs={12} md={6} lg={3} className="phone-block"
+        onClick={() => dispatch(changeBrand('alcatel'))}>
+            <div style={{margin: '20px'}}>
+              <Image src="/front/alcatel.png" width="85px" height="40px" />
+            </div>
+            <div style={{margin: '20px'}}>
+              <Image src="/front/display_img_5.png" width="140px" height="140px" />
+            </div>
+        </Grid>
+
+
+        <Grid item xs={12} md={6} lg={3} className="phone-block"
+         onClick={() => dispatch(changeBrand('google'))}>
+            <div style={{margin: '20px'}}>
+              <Image src="/front/pixel.png" width="70px" height="45px" />
+            </div>
+            <div style={{margin: '20px'}}>
+              <Image src="/front/display_img_6.png" width="140px" height="140px" />
+            </div>
+        </Grid>
+
+
+        
+        <Grid item xs={12} md={6} lg={3} className="phone-block"
+         onClick={() => dispatch(changeBrand('vivo'))}>
+            <div style={{margin: '20px'}}>
+              <Image src="/front/vivo.png" width="75px" height="40px" />
+            </div>
+            <div style={{margin: '20px'}}>
+              <Image src="/front/display_img_7.png" width="140px" height="140px" />
+            </div>
+        </Grid>
+
       </Grid>
-      {BlockThree("/front/htc.png", "/front/display_img_4.png")}
-      {BlockThree("/front/alcatel.png", "/front/display_img_5.png")}
-      {BlockThree("/front/pixel.png", "/front/display_img_6.png")}
-      {BlockThree("/front/vivo.png", "/front/display_img_7.png")}
-    </Grid>
+    </Link>
   )
 }
 
