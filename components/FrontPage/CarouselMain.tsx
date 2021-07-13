e1f59ad3b80d94d0087ea8d3a73f8c4493fc9e42
Veljko28 <charlibear28@gmail.com>
2021-07-13 13:09:41 +0200

    fixing the carousel interval duration

--- components/FrontPage/CarouselMain.tsx
+++ components/FrontPage/CarouselMain.tsx
@@ -9,12 +9,11 @@ import { Button } from '@material-ui/core';
 
 const CarouselMain = () => {
   return (
-  <Carousel
-   interval={3000}
+  <Carousel slide={true} 
    nextIcon={<ArrowForwardIosIcon style={{fontSize: '25px', color: 'black'}}/>}
    prevIcon={<ArrowBackIosIcon style={{fontSize: '25px', color: 'black'}}/>}>
 
-    <Carousel.Item interval={1000}>
+    <Carousel.Item interval={3000}>
       <Image
         src="/slider/slider1.jpg"
         alt="First slide"
@@ -26,12 +25,13 @@ const CarouselMain = () => {
         <div className="text-left">
           <Image src="/slider/mi_logo.png" width="40px" height="40px"/>
           <h3 style={{color: 'black'}}>Red Mi Y1</h3>
-          <p style={{color: 'black'}}>LED Selfie-light | Fingerprint sensor | Dedicated microSD card slot Snapdragon 435 octa-core processor</p>
+          <p style={{color: 'black'}}>LED Selfie-light | Fingerprint sensor 
+          <br/> Dedicated microSD card slot Snapdragon 435 octa-core processor</p>
           <Button variant="contained" style={{padding: '20px', backgroundColor: '#ff4a00', color: '#fff'}}>BUY NOW !</Button>
         </div>
       </Carousel.Caption>
     </Carousel.Item>
-    <Carousel.Item interval={500}>
+    <Carousel.Item interval={3000}>
       <Image
         src="/slider/slider2.jpg"
         alt="Second slide"
@@ -43,12 +43,13 @@ const CarouselMain = () => {
         <div className="text-left">
           <Image src="/slider/google_logo.png" width="40px" height="40px"/>
           <h3 style={{color: 'black'}}>Google Pixel 2</h3>
-          <p style={{color: 'black'}}>The latest Qualcomm Snapdragon 835 processor | Water-resistant metal unibody | Up to 7 hours of battery.</p>
+          <p style={{color: 'black'}}>The latest Qualcomm Snapdragon 835 processor 
+          <br/> Water-resistant metal unibody | Up to 7 hours of battery.</p>
           <Button variant="contained" style={{padding: '20px', backgroundColor: '#ff4a00', color: '#fff'}}>BUY NOW !</Button>
         </div>
       </Carousel.Caption>
     </Carousel.Item>
-    <Carousel.Item>
+    <Carousel.Item interval={3000}>
       <Image
         src="/slider/slider3.jpg"
         alt="Third slide"
