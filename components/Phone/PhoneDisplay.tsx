3008d76ec24a4f6171de4e3ea021475b8916f010
Veljko28 <charlibear28@gmail.com>
2021-07-13 13:13:08 +0200

    modefing the phonedisplay for bids adding seller info and bid history start

--- components/Phone/PhoneDisplay.tsx
+++ components/Phone/PhoneDisplay.tsx
@@ -1,14 +1,25 @@
-import { Button, Grid, Typography } from "@material-ui/core";
-import { useSelector, useDispatch } from "react-redux";
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { Button, Dialog, DialogActions, DialogContent,
+DialogContentText, DialogTitle, Grid, Typography } from "@material-ui/core";
 import Rating from '@material-ui/lab/Rating';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import HistoryIcon from '@material-ui/icons/History';
+
 import Image from "next/image";
 import React from 'react';
 
+import { useSelector, useDispatch } from "react-redux";
 import { Phone } from "../models/Phone";
 import { State } from "../../redux/reduxTypes";
 import { changeQuantity } from "../../redux/actions/phonesActions";
 import { addToCart } from "../../redux/actions/cartActions";
+
+const Alert = (props: any) => {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const ImageMapper = (images: string[],changeCurrentImage: React.Dispatch<React.SetStateAction<string>>) => {
     return images.map(x => (
@@ -18,15 +29,30 @@ const ImageMapper = (images: string[],changeCurrentImage: React.Dispatch<React.S
     ))
 }
 
-const PhoneDisplay = ({phone} : {phone?: Phone}) => {
+const PhoneDisplay = ({phone,bid} : {phone?: Phone, bid?: boolean}) => {
   // Temp variables for testing
-  const price = "1500$";
-  const discount = "1200$";
+  const discount = bid ? null : "1200$" ;
 
   const quantity = useSelector((state: State) => state.phones.quantity);
   const dispatch = useDispatch();
 
+  // only for testing
+  const [price,changePrice] = React.useState("250$");
+  const name = "Google Pixel";
+
   const [currentImage,changeCurrentImage] = React.useState('/phone.jpg');
+  const [bidAmount,changeBidAmount] = React.useState(parseInt(price.slice(0,-1))+1);
+  const [dialogOpen,changeDialogOpen] = React.useState(false);
+  const [snackBarOpen,changeSnackBarOpen] = React.useState(false);
+
+
+  const bidConfimed = () => {
+    if (bidAmount !== null && bidAmount > parseInt(price.slice(0,-1))+1){
+      changePrice(bidAmount+'$');
+      changeSnackBarOpen(true);
+    }
+    changeDialogOpen(false);
+  }
 
   return (
     <Grid container className="display-container">
@@ -42,7 +68,7 @@ const PhoneDisplay = ({phone} : {phone?: Phone}) => {
       </Grid>
 
       <Grid item style={{padding: '20px'}}>
-        <Typography variant='subtitle1' style={{fontSize: '20px'}}>Google Pixel</Typography>
+        <Typography variant='subtitle1' style={{fontSize: '20px'}}>{name}</Typography>
         <div className="phone-rating">
           <Rating name="phone-rating" value={4.6} precision={0.1} readOnly style={{fontSize: '13px'}}/>
           <span style={{color: '#999', fontSize: '12px',margin: '5px' }}>(4.6 Review Stars)</span>
@@ -53,11 +79,25 @@ const PhoneDisplay = ({phone} : {phone?: Phone}) => {
           <span style={{color: '#0cafe5', fontSize: "35px"}}>{discount} </span>
           <span style={{color: 'red', textDecoration: 'line-through', fontSize: "20px"}}>{price}</span>
           </>) : 
-          (<span style={{color: '#0cafe5', fontSize: "35px"}}>{price}</span>)
+          (<span style={{color: '#0cafe5', fontSize: "35px"}}>{bid ? "Highest Bid: " : ""}{price}</span>)
           }
           <span style={{color: '#999', fontSize: '15px', display: 'flex', width: '400px'}}>
             12.2 MP Rear | 8 MP Front Camera, 4GB RAM, 2700 mAh battery, Android 8.0, Qualcomm Snapdragon 835, Fingerprint Sensor
           </span>
+          {bid ? (<>
+           <span style={{fontSize: '12px'}}>
+            Your Bid
+          </span>
+          <input type="number"
+          value={bidAmount}
+          onChange={e => changeBidAmount(e.target.value as any)}
+          min={parseInt(price.slice(0,-1))+1}
+          className="bid-price"/>
+          <Button variant="contained" onClick={() => changeDialogOpen(true)}
+          style={{backgroundColor: '#0cafe5', color: '#fff', padding: '15px', marginTop: '10px'}}>
+            <MonetizationOnIcon style={{fontSize: '20px', marginRight: '5px'}}/>Bid {bidAmount}$</Button>
+           <div className="bid-history"><HistoryIcon style={{fontSize: '20px', marginRight: '5px'}}/>Bid History (3)</div>
+          </>) : (<> 
           <span style={{fontSize: '12px'}}>
             Quantity
           </span>
@@ -66,9 +106,47 @@ const PhoneDisplay = ({phone} : {phone?: Phone}) => {
           onClick={() => dispatch(addToCart({item: phone, amount: quantity}))}
           style={{backgroundColor: '#0cafe5', color: '#fff', padding: '15px', marginTop: '10px'}}>
             <ShoppingCartIcon style={{fontSize: '20px', marginRight: '5px'}}/> ADD TO CART</Button>
+          </>)}
         </Typography>
         
       </Grid>
+
+
+
+      <Dialog
+        open={dialogOpen}
+        onClose={() => changeDialogOpen(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{`Are you sure you want to bid ${bidAmount}$ ?`}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Bid only the amount of money you are willing to give for the phone.
+            Agreeing with this will put you in the position to win this bid. You won't be able to undo 
+            this bid after agreeing !  
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => changeDialogOpen(false)} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={() => bidConfimed()} color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+
+
+      <Snackbar 
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      open={snackBarOpen} autoHideDuration={5000} onClose={() => changeSnackBarOpen(false)}>
+        <Alert onClose={() => changeSnackBarOpen(false)} severity="success">
+          Successfully placed a bid of {bidAmount}$ on {name}
+        </Alert>
+      </Snackbar>
+
     </Grid>
   )
 }
---
+++ components/Phone/SellerInfo.tsx
@@ -0,0 +1,55 @@
+import { Grid, Typography, Button } from '@material-ui/core';
+import ColoredLine from '../../constants/ColoredLine';
+import Rating from '@material-ui/lab/Rating';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
+
+
+const SellerInfo = ({id} : {id: string}) => {
+
+    return (
+        <Grid className="phone-details" container>
+             <Typography variant="h6" style={{margin: '10px', marginLeft: '40px',
+        color: '#0cafe5'}}>About The Seller</Typography>
+            <ColoredLine color="#eee"/>
+            <Grid xs={12} container item>
+             <Link href={`/user/${id}`}>
+                <Grid xs={12} md={4} item className="review-grid-item">
+                  <div className="curs-hvr">
+                    <Image src="/user.png" width="70px" height="70px" />
+                    <div>Seller Username</div>
+                  </div>
+                </Grid>
+              </Link>
+                <Grid xs={12} md={4} item className="review-grid-item">
+                    <Typography variant="subtitle1">
+                        Rating: <Rating name="seller-rating" value={4.5} precision={0.1} readOnly
+                     style={{fontSize: '16px', margin: '10px', marginTop: '15px'}}/>
+                    </Typography>
+                    <Typography variant="subtitle1">
+                        Phones Sold: <span style={{color: '#0cafe5'}}>226</span>
+                        <PhoneAndroidIcon style={{fontSize: '20px', color: '#0cafe5',marginBottom: '5px'}}/>
+                    </Typography>
+                    <Typography variant="subtitle1">
+                        Currently Selling: <span style={{color: '#0cafe5'}}>27</span>
+                        <PhoneAndroidIcon style={{fontSize: '20px', color: '#0cafe5',marginBottom: '5px'}}/>
+                    </Typography>
+                </Grid>
+                <Grid xs={12} md={4} item className="review-grid-item">
+                    <Button variant="contained" 
+                    style={{color: '#fff', backgroundColor: '#0cafe5', padding: '10px', width: '175px', margin: '5px'}}
+                    >Contact The Seller</Button>
+                    <Link href={`/user/${id}/listings`}>
+                      <Button variant="contained" 
+                      style={{color: '#fff', backgroundColor: '#0cafe5', padding: '10px', width: '175px', margin: '5px'}}
+                      >View Listings</Button>
+                    </Link>
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+}
+
+export default SellerInfo;
