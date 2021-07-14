a88900c794261a08c445677ca1f70ef1c5156880
Veljko28 <charlibear28@gmail.com>
2021-07-14 13:11:04 +0200

    adding user tabs with styles and no functionality

---
+++ components/User/UserTabs.tsx
@@ -0,0 +1,35 @@
+import { Button } from '@material-ui/core'
+import React from 'react'
+
+const UserTabs = () => {
+
+  const selectedStyle = {color: '#0cafe5', borderBottom: '2px solid #0cafe5', borderRadius: 0, marginRight: 10};
+  const normalStyle = {color: '#0cafe5', borderRadius: 0, paddingBottom: 10,  marginRight: 10};
+
+  const [selected,changeSelected] = React.useState('Listings');
+
+  return (
+    <div style={{margin: '10px'}}>
+      <Button 
+      style={selected === 'Listings' ? selectedStyle : normalStyle}
+      onClick={() => {
+        changeSelected('Listings');
+      }}
+      >Listings</Button>
+      <Button 
+      style={selected === 'Reviews' ? selectedStyle : normalStyle}
+      onClick={() => {
+        changeSelected('Reviews');
+      }}
+      >Reviews</Button>
+      <Button 
+      style={selected === 'Wish List' ? selectedStyle : normalStyle}
+        onClick={() => {
+        changeSelected('Wish List');
+      }}
+      >Wish List</Button>
+    </div>
+  )
+}
+
+export default UserTabs
