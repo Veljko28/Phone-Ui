93204c004385f49845b9e514d90a7a6b5133af9f
Veljko28 <charlibear28@gmail.com>
2021-07-25 13:29:40 +0200

    custom snackbars

---
+++ constants/CustomSnackBars.tsx
@@ -0,0 +1,29 @@
+import { Snackbar } from "@material-ui/core";
+import MuiAlert from '@material-ui/lab/Alert';
+import React from "react";
+
+const Alert = (props: any) => {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+
+export const SnackBarSuccess = (snackBarOpen: boolean, changeSnackBarOpen: React.Dispatch<React.SetStateAction<boolean>>, message: string) => {
+   <Snackbar 
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      open={snackBarOpen} autoHideDuration={5000} onClose={() => changeSnackBarOpen(false)}>
+        <Alert onClose={() => changeSnackBarOpen(false)} severity="success">
+          {message}
+        </Alert>
+     </Snackbar>
+}
+
+
+export const SnackBarFailed = (snackBarOpen: boolean, changeSnackBarOpen: React.Dispatch<React.SetStateAction<boolean>>, message: string) => {
+   <Snackbar 
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      open={snackBarOpen} autoHideDuration={5000} onClose={() => changeSnackBarOpen(false)}>
+        <Alert onClose={() => changeSnackBarOpen(false)} severity="error">
+          {message}
+        </Alert>
+     </Snackbar>
+}
