7cb9a5bd296ca9c2201e14fe609a79769590c5a8
Veljko28 <charlibear28@gmail.com>
2021-07-27 11:05:00 +0200

    fetching from the api and sending info

--- constants/CustomFetching.ts
+++ constants/CustomFetching.ts
@@ -1,20 +1,65 @@
+export const fetchPostBid = async (url: string, payload: any) => {
+    console.log(payload);
 
-export const fetchPost = async (url: string, payload: any) => {
     const str = JSON.stringify(payload);
 
-    const res = await fetch(url, {
+    const res: any = await fetch(url, {
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
       },
       body: str,
       method: 'POST',
-    });
+    }).catch((error) => {
+      console.log(error);
+    })
 
-    console.log(res.ok);
-    return res.ok;
+    const bid = await res.json();
+    return bid?.id;
 }
 
+export const fetchPost = async (url: string, payload: any) => {
+    const str = JSON.stringify(payload);
+
+    const res: any = await fetch(url, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: str,
+      method: 'POST',
+    }).catch((error) => {
+      console.log(error);
+      return false;
+    })
+
+    const phone = await res.json();
+    return phone?.id;
+}
+
+
+export const fetchPostForm = async (url: string, payload: any, type: number, id: string) => {
+
+    // Fix sending files to fetch
+
+      const data = new FormData();
+      for (const file of payload){
+        data.append("Files", file);
+      }
+      data.append("Type", type.toString());
+      data.append("Id", id);
+
+
+    const res: any = await fetch(url, {
+      body: data,
+      method: 'POST',
+    }).catch((error) => {
+      console.log(error);
+      return false;
+    })
+
+    return res.ok;
+}
 
 export const fetchGet = (url: string) => {
   fetch(url, { method: 'GET',}).then(res => {return res;})
