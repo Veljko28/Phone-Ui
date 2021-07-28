b62e28faf52cd22c9b7160b372f2d5751536c03a
Veljko28 <charlibear28@gmail.com>
2021-07-28 13:35:37 +0200

    removing register and login redux and fixing the store

--- redux/actions/loginActions.ts
+++
@@ -1,7 +0,0 @@
-import templateAction from "../../constants/templateAction";
-
-export const INPUT_EMAIL_USERNAME = "INPUT_EMAIL_USERNAME";
-export const INPUT_PASSWORD = "INPUT_PASSWORD";
-
-export const inputEmailUsername = templateAction(INPUT_EMAIL_USERNAME);
-export const inputPassword = templateAction(INPUT_PASSWORD);
--- redux/store.ts
+++ redux/store.ts
@@ -3,8 +3,6 @@ import cartReducers from './reducers/cartReducers';
 import langReducers from './reducers/langReducers';
 import phoneReducers from './reducers/phoneReducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import loginReducers from './reducers/loginReducers';
-import registerReducers from './reducers/registerReducers';
 import contactReducers from './reducers/contactReducers';
 import { userInfoReducers } from './reducers/userInfoReducers';
 
@@ -13,8 +11,6 @@ const rootReducer = combineReducers({
     language: langReducers,
     cart: cartReducers,
     phones: phoneReducers,
-    login: loginReducers,
-    register: registerReducers,
     contact: contactReducers,
     userInfo: userInfoReducers,
 });
