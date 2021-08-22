1dc8631754846541120b09a7dffd6cba200a9c2e
Veljko28 <charlibear28@gmail.com>
2021-08-22 09:18:55 +0200

    confirming email if ids match and removing the confirm email notification

--- pages/confirmemail/[id].tsx
+++ pages/confirmemail/[id].tsx
@@ -5,19 +5,54 @@ import { useRouter } from 'next/router'
 import { Grid, Typography, Button } from '@material-ui/core';
 import {blue, dark_gray, white} from '../../constants/CustomColors';
 import NotFound from '../../components/NotFound';
+import { fetchGet, fetchPost } from '../../constants/CustomFetching';
 
 const ConfirmEmail = () => {
 
   const router = useRouter();
 
   const id = router.query['id'];
-  const valid = true;
+  const [valid, changeValid] = React.useState<boolean | null>(null);
+  let confirmEmailId: string | null = "";
+  let userId: string | null = "";
+
+
+  if (typeof window !== 'undefined'){
+    confirmEmailId = localStorage.getItem('confirmEmailId');
+    userId = localStorage.getItem('userId');
+    if (confirmEmailId == null || userId == null) {
+      changeValid(false);
+    }
+  }
+
+  React.useEffect(() => {
+    const func = async () => {
+      if (id != confirmEmailId) changeValid(false);
+      const res = await fetchGet(`http://localhost:10025/api/v1/generic/confirmemail/${userId}`);
+
+      if (res.ok){
+        changeValid(true);
+
+        // removing notification for confirming email
+        await fetchPost('http://localhost:10025/api/v1/notifications/remove', {
+          userId,
+          type: "email",
+          name: "",
+          message: ""
+        })
+      }
+      else changeValid(false);
+
+    }
+
+    if (id && confirmEmailId && userId) func();
+  }, [id]) 
 
   return valid ? (
     <Grid container 
     style={{display: 'flex', flexDirection: 'column', 
     alignItems: 'center',justifyContent: 'center',
-    backgroundColor: white, minHeight: 450}}>
+    backgroundColor: white, minHeight: 650}}>
       <Image src="/logo.png" width="180px" height="50px"/>
       <Typography variant="h5" style={{color: blue, marginTop: 20}}>Successfully confirmed your email address !</Typography>
       <Typography variant="subtitle2" style={{color: dark_gray, marginTop: 10, marginBottom: 20, textAlign: 'center'}}>
