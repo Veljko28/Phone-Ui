cdb26c9bc426beb9043641b392912acceb73a69e
Veljko28 <charlibear28@gmail.com>
2021-08-28 13:28:28 +0200

    fixing the helper bar and adding the dark mode functionality

--- components/HeaderComps/HelpBar.tsx
+++ components/HeaderComps/HelpBar.tsx
@@ -5,6 +5,7 @@ import { State } from '../../redux/reduxTypes';
 import { useSelector } from 'react-redux';
 import LanguagePopUp from './LanguagePopUp';
 import { dark_gray } from '../../constants/CustomColors';
+import AnimatedIcon from './AnimatedIcon';
 
 
 
@@ -12,6 +13,7 @@ import { dark_gray } from '../../constants/CustomColors';
 const HelpBar = () => {
   
   const lang = useSelector((state: State) => state.language.lang);
+  const darkMode = useSelector((state: State) => state.userInfo.darkMode);
 
 
   const [langAnchorEl, setLangAnchorEl] = React.useState(null);
@@ -26,18 +28,19 @@ const HelpBar = () => {
   }
 
   return (
-    <Grid container className="helper-container">
+    <Grid container className={darkMode ? "helper-container-dark" : "helper-container"}>
       <Grid item xs={12} lg={4} style={{display: 'flex', justifyContent: 'center'}}>
         <div style={{fontSize: 12, marginTop: 5, color: dark_gray, marginLeft: 25}}>Best Online Mobile Selling Store !</div>
       </Grid>
       <Grid item xs={12} lg={8} className="top-grid">
-        <div className="support">support: +381 063 800-3210 </div>
-        <div className="support">support@mobistore.com</div>
+        <div className={darkMode ? "support-dark" : "support"}>support: +381 063 800-3210 </div>
+        <div className={darkMode ? "support-dark" : "support"}>support@mobistore.com</div>
         <div onClick={e => openLang(e)} className="flag-support">
          <Image src={lang == 'en' ? '/lang/en.png' : '/lang/sr.png'} 
               alt={lang == 'en' ? "English" : "Српски"} width="20" height="15"  />
         </div>
         <LanguagePopUp open={langOpen} handleClose={() => closeLang()} anchorEl={langAnchorEl}/>
+        <AnimatedIcon/>
       </Grid>
     </Grid>
   )
