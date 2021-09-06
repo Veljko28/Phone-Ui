988897524c3dfc6d050f06bb0f744bab9b76118b
Veljko28 <charlibear28@gmail.com>
2021-09-06 11:04:40 +0200

    adding fixed phone display skeleton

--- components/Skeletons/PhoneDisplaySkeleton.tsx
+++ components/Skeletons/PhoneDisplaySkeleton.tsx
@@ -5,15 +5,16 @@ import React from 'react'
 
 const PhoneDisplaySkeleton = ({darkMode} : {darkMode: boolean}) => {
   return (
-    <Grid container className={darkMode ? "display-container-dark" : "display-container"} style={{height: 525, marginTop: 20, marginBottom: 20}}>
-      <Grid item className="other-images" xs={12} md={6} lg={2}>
-        <Skeleton  width="55px" height="75px"/>
-        <Skeleton  width="55px" height="75px"/>
-        <Skeleton  width="55px" height="75px"/>
-      </Grid>
-
-      <Grid item xs={12} md={6} lg={2}>
-          <Skeleton width="350px" height="550px" style={{position: 'absolute', top: 75, left: 450}}/>
+    <Grid container className={darkMode ? "display-container-dark" : "display-container"} style={{minHeight: 525, marginTop: 20, marginBottom: 20}}>
+      <Grid item xs={12} md={6} lg={4} style={{display: 'flex'}}>
+        <div className="other-images">
+          <Skeleton  width="55px" height="75px"/>
+          <Skeleton  width="55px" height="75px"/>
+          <Skeleton  width="55px" height="75px"/>
+        </div>
+        <div style={{margin: 20}}>
+          <Skeleton width="325px" height="325px" variant="rect"/>
+        </div>
       </Grid>
 
       <Grid item style={{padding: '20px'}} md={12} lg={8}>
@@ -22,9 +23,9 @@ const PhoneDisplaySkeleton = ({darkMode} : {darkMode: boolean}) => {
               <Skeleton  width="150px" height="30px"/>
               <Skeleton  width="200px" height="20px"/>
               <Skeleton  width="200px" height="70px"/>
-              <Skeleton  width="400px" variant="text"/>
-              <Skeleton  width="400px" variant="text"/>
-              <Skeleton  width="400px" variant="text"/>
+              <Skeleton  width="80%" variant="text"/>
+              <Skeleton  width="80%" variant="text"/>
+              <Skeleton  width="80%" variant="text"/>
               <Skeleton  width="150px" height="90px"/>
               <div style={{display: 'flex'}}>
                 <Skeleton  style={{borderRadius: '35%', width: 60, height: 50, marginRight: 10}}/>
